002975a3feafc44dc7e190a33d5949b8c81f026c
Adam <akirbsy@gmail.com>
2021-12-28 18:09:52 -0800

    Update IDConstants.js
    added identifiers for every job (hopefully)

--- src/lib/IDConstants.js
+++ src/lib/IDConstants.js
@@ -1,6 +1,23 @@
 export const JobIds = {
+  19: 'PLD',
+  20: 'MNK',
+  21: 'WAR',
+  22: 'DRG',
+  23: 'BRD',
+  24: 'WHM',
   25: 'BLM',
+  27: 'SMN',
+  28: 'SCH',
+  30: 'NIN',
+  32: 'DRK',
+  33: 'AST',
+  34: 'MCH',
   35: 'RDM',
+  36: 'SAM',
+  37: 'GNB',
+  38: 'DNC',
+  39: 'RPR',
+  40: 'SGE',
 }
 
 export const ACTION_TYPES = {
