ee58cfa87b726d3a95ee7a7f5feab3aa69e14435
Adam <akirbsy@gmail.com>
2021-12-29 02:41:11 -0800

    added reaper support and changed the icon storage layout
    reaper works mostly, one issue with the actionreader, but the rest is good to go
    
    split role actions away from the jobs (we dont need 4 copies of the tank role action folder lol) and fixed the "roll action" typo

---
+++ src/lib/phases/RPR/opener.js
@@ -0,0 +1,42 @@
+import { ACTION_TYPES } from '../../IDConstants'
+// Reaper Fast Shroud Opener
+const opener = []
+
+opener.push({ name: 'Soulsow', type: ACTION_TYPES.GCD, note: 'whenever' })
+opener.push({ name: 'Harpe', type: ACTION_TYPES.GCD, note: '-2s' })
+
+//  Pull
+opener.push({ name: 'Pull', type: ACTION_TYPES.PULL })
+
+opener.push({ name: 'Shadow of Death', type: ACTION_TYPES.GCD })
+opener.push({ name: 'Arcane Circle', type: ACTION_TYPES.oGCD })
+
+opener.push({ name: 'Soul Slice', type: ACTION_TYPES.GCD })
+opener.push({ name: 'Tincture of Strength', type: ACTION_TYPES.CONSUMABLE })
+
+opener.push({ name: 'Soul Slice', type: ACTION_TYPES.GCD })
+
+opener.push({ name: 'Plentiful Harvest', type: ACTION_TYPES.GCD })
+opener.push({ name: 'Enshroud', type: ACTION_TYPES.oGCD })
+
+opener.push({ name: 'Void Reaping', type: ACTION_TYPES.GCD })
+
+opener.push({ name: 'Cross Reaping', type: ACTION_TYPES.GCD })
+opener.push({ name: 'Lemure\'s Slice', type: ACTION_TYPES.oGCD })
+
+opener.push({ name: 'Void Reaping', type: ACTION_TYPES.GCD })
+
+opener.push({ name: 'Cross Reaping', type: ACTION_TYPES.GCD })
+opener.push({ name: 'Lemure\'s Slice', type: ACTION_TYPES.oGCD })
+
+opener.push({ name: 'Communio', type: ACTION_TYPES.GCD })
+opener.push({ name: 'Gluttony', type: ACTION_TYPES.oGCD })
+
+opener.push({ name: 'Gibbet', type: ACTION_TYPES.GCD })
+
+opener.push({ name: 'Gallows', type: ACTION_TYPES.GCD })
+opener.push({ name: 'Unveiled Gibbet', type: ACTION_TYPES.oGCD })
+
+opener.push({ name: 'Gibbet', type: ACTION_TYPES.GCD })
+
+export default opener
